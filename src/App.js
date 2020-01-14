55c5cbd1f6020ec19d62aa0240c2d75761b80af4
Bishal Udash <supertramp.bishal@gmail.com>
2020-01-14 08:57:01 +0545

    components, props lesson

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, {Component} from 'react';
+import Navbar from './components/navbar/Navbar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h2> Hello world</h2>
-    </div>
-  );
+class App extends Component{
+  render(){
+    const name = "Bishal Udash";
+    const getAge =() => "24";
+    const loading = false;
+
+    return (
+      <div className="App">
+        <Navbar />
+
+        <div>
+          {loading ?  "Loading"  : "Hello "+ name.toUpperCase()}
+        </div>
+        <p>{getAge()}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
